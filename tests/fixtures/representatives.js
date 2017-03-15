cdc09fdbd6e6f7d396548bf1d7c9cd78d210b5b1
Brian Whitton <me@brianwhitton.com>
2017-03-14 21:29:13 -0400

    adds google civics client

---
+++ tests/fixtures/representatives.js
@@ -0,0 +1,209 @@
+module.exports = {
+ "offices": [
+  {
+   "name": "President of the United States",
+   "officialIndices": [
+    0
+   ]
+  },
+  {
+   "name": "Vice-President of the United States",
+   "officialIndices": [
+    1
+   ]
+  },
+  {
+   "name": "United States Senate",
+   "officialIndices": [
+    2,
+    3
+   ]
+  },
+  {
+   "name": "United States House of Representatives NY-07",
+   "officialIndices": [
+    4
+   ]
+  },
+  {
+   "name": "Governor",
+   "officialIndices": [
+    5
+   ]
+  },
+  {
+   "name": "Lieutenant Governor",
+   "officialIndices": [
+    6
+   ]
+  },
+  {
+   "name": "NY State Senate District 25",
+   "officialIndices": [
+    7
+   ]
+  },
+  {
+   "name": "NY State Assembly District 52",
+   "officialIndices": [
+    8
+   ]
+  },
+  {
+   "name": "Mayor",
+   "officialIndices": [
+    9
+   ]
+  },
+  {
+   "name": "Comptroller",
+   "officialIndices": [
+    10
+   ]
+  },
+  {
+   "name": "Public Advocate",
+   "officialIndices": [
+    11
+   ]
+  },
+  {
+   "name": "Attorney General",
+   "officialIndices": [
+    12
+   ]
+  },
+  {
+   "name": "State Comptroller",
+   "officialIndices": [
+    13
+   ]
+  },
+  {
+   "name": "County District Attorney",
+   "officialIndices": [
+    14
+   ]
+  },
+  {
+   "name": "Brooklyn Borough President",
+   "officialIndices": [
+    15
+   ]
+  }
+ ],
+ "officials": [
+  {
+   "name": "Donald J. Trump",
+   "party": "Republican",
+   "phones": [
+    "(202) 456-1111"
+   ]
+  },
+  {
+   "name": "Mike Pence",
+   "party": "Republican",
+   "phones": [
+    "(202) 456-1111"
+   ]
+  },
+  {
+   "name": "Charles E. Schumer",
+   "party": "Democratic",
+   "phones": [
+    "(202) 224-6542"
+   ]
+  },
+  {
+   "name": "Kirsten E. Gillibrand",
+   "party": "Democratic",
+   "phones": [
+    "(202) 224-4451"
+   ]
+  },
+  {
+   "name": "Nydia M. Velazquez",
+   "party": "Democratic",
+   "phones": [
+    "(202) 225-2361"
+   ]
+  },
+  {
+   "name": "Andrew M. Cuomo",
+   "party": "Democratic",
+   "phones": [
+    "(518) 474-8390"
+   ]
+  },
+  {
+   "name": "Kathy Hochul",
+   "party": "Democratic",
+   "phones": [
+    "(518) 474-8390"
+   ]
+  },
+  {
+   "name": "Velmanette Montgomery",
+   "party": "Democratic",
+   "phones": [
+    "(518) 455-3451"
+   ]
+  },
+  {
+   "name": "Jo Anne Simon",
+   "party": "Democratic",
+   "phones": [
+    "(518) 455-5426"
+   ]
+  },
+  {
+   "name": "Bill de Blasio",
+   "party": "Democratic",
+   "phones": [
+    "(212) 639-9675"
+   ]
+  },
+  {
+   "name": "Scott M. Stringer",
+   "party": "Democrat",
+   "phones": [
+    "(212) 669-3916"
+   ]
+  },
+  {
+   "name": "Letitia \"Tish\" James",
+   "party": "Democratic",
+   "phones": [
+    "(212) 669-7200"
+   ]
+  },
+  {
+   "name": "Eric T. Schneiderman",
+   "party": "Democratic",
+   "phones": [
+    "(800) 771-7755"
+   ]
+  },
+  {
+   "name": "Thomas P. DiNapoli",
+   "party": "Democratic",
+   "phones": [
+    "(518) 474-4044"
+   ]
+  },
+  {
+   "name": "Kenneth P. Thompson",
+   "party": "Unknown",
+   "phones": [
+    "(718) 250-2000"
+   ]
+  },
+  {
+   "name": "Eric Adams",
+   "party": "Unknown",
+   "phones": [
+    "(718) 802-3700"
+   ]
+  }
+ ]
+}
---
+++ tests/google-civics-test.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var expect = chai.expect;
+var googleCivics = require('../lib/clients/google-civics');
+var sinon = require('sinon');
+var request = require('request');
+var REPS_RESPONSE = require('./fixtures/representatives');
+
+describe('Google Civics API Client', function() {
+  it('should make a request to google civics with the given address', function(done) {
+    const ADDRESS = "123 Main St, Anytown, OH";
+    
+    var getSpy = sinon.stub(request, 'get', (a, b) => {
+      b(undefined, {}, REPS_RESPONSE);
+    });
+    
+    googleCivics.getReps(ADDRESS).then(res => {
+      expect(res).to.deep.equal(REPS_RESPONSE);
+      expect(getSpy.calledWith({
+        url: "https://www.googleapis.com/civicinfo/v2/representatives",
+        qs: {
+          key: process.env.GOOGLE_API_KEY,
+          address: ADDRESS,
+          fields: 'offices(name,officialIndices),officials(name,party,phones)'
+        },
+        json: true
+      })).to.be.ok;
+      
+      getSpy.restore();
+      done();
+    });
+      
+  });
+});
